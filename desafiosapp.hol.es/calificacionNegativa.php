39b646ece19d384511fdc422b3214f0c7de71bf1
BRUNO TOBIAS VOLCOVINSKY <42302359@ort>
2017-09-28 15:37:31 -0300

    Calificaciones

--- desafiosapp.hol.es/calificacionNegativa.php
+++ desafiosapp.hol.es/calificacionNegativa.php
@@ -18,19 +18,19 @@ if($_SERVER['REQUEST_METHOD']=="POST") {
 			if($consulta->num_rows == 1) {
 				//Publicacion ok
 
-				//Ahora hay que ver si ya lo habia calificado positivamente
-				$consulta = $con->query("SELECT IDCALIFICACION FROM calificaciones WHERE IDUSUARIO='$idusuario' AND IDPUBLICACION='$idPublicacion' AND CALIFICACION='1' LIMIT 1");
+				//Ahora hay que ver si ya lo habia calificado negativamente
+				$consulta = $con->query("SELECT IDCALIFICACION FROM calificaciones WHERE IDUSUARIO='$idusuario' AND IDPUBLICACION='$idPublicacion' AND CALIFICACION='0' LIMIT 1");
 
 				if($consulta->num_rows == 1) {
 					echo "Ya lo habia calificado";
-					// Ya lo habia calificado positivamente, por lo tanto, hay que sacar esa calificacion
-					$con->query("DELETE FROM calificaciones WHERE IDUSUARIO='$idusuario' AND IDPUBLICACION='$idPublicacion' AND CALIFICACION='1' LIMIT 1");
+					// Ya lo habia calificado negativamente, por lo tanto, hay que sacar esa calificacion
+					$con->query("DELETE FROM calificaciones WHERE IDUSUARIO='$idusuario' AND IDPUBLICACION='$idPublicacion' AND CALIFICACION='0' LIMIT 1");
 				} else {
 					echo "No lo habia calificado";
-					//No lo habia calificado, lo califica y por las dudas borro un posible negativo
-					$con->query("INSERT INTO calificaciones (IDPUBLICACION, IDUSUARIO, CALIFICACION) VALUES ('$idPublicacion', '$idusuario', '1')");
+					//No lo habia calificado, lo califica y por las dudas borro un posible positivo
+					$con->query("INSERT INTO calificaciones (IDPUBLICACION, IDUSUARIO, CALIFICACION) VALUES ('$idPublicacion', '$idusuario', '0')");
 					
-					$con->query("DELETE FROM calificaciones WHERE IDUSUARIO='$idusuario' AND IDPUBLICACION='$idPublicacion' AND CALIFICACION='0' LIMIT 1");
+					$con->query("DELETE FROM calificaciones WHERE IDUSUARIO='$idusuario' AND IDPUBLICACION='$idPublicacion' AND CALIFICACION='1' LIMIT 1");
 
 				}
 
